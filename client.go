4b33862fac1706b8c0a4328e3072e51c39848547
lijinyang <kylin27@126.com>
2019-12-07 10:58:04 +0800

    init

---
+++ client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"github.com/micro/go-plugins/registry/etcdv3"
+	model "proto"
+	"fmt"
+)
+
+func main() {
+	// 我这里用的etcd 做为服务发现，如果使用consul可以去掉
+	reg := etcdv3.NewRegistry(func(op *registry.Options) {
+		op.Addrs = []string{
+			"http://192.168.3.34:2379", "http://192.168.3.18:2379", "http://192.168.3.110:2379",
+		}
+	})
+
+	// 初始化服务
+	service := micro.NewService(
+		micro.Registry(reg),
+	)
+	service.Init()
+	sayClent := model.NewSayService("lp.srv.eg1", service.Client())
+
+	rsp, err := sayClent.Hello(context.Background(), &model.SayParam{Msg: "hello server"})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(rsp)
+
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module micro-etcd
+
+go 1.13
+
+replace model => F:\GoWorkSpace\micro-etcd\proto
+
+require (
+	github.com/micro/go-plugins v1.5.1 // indirect
+	model v0.0.0-00010101000000-000000000000 // indirect
+)
